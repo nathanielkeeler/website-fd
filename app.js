75f3b3bc65f61bc235b72d7a4f5b137a98218256
Nathan <nathaniel.hermann.keeler@hs-furtwangen.de>
2023-02-24 12:24:00 +0100

    image cropping

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const rootElement = document.documentElement;
 const scrollToTopButton = document.querySelector("#scrollToTopBtn");
 
-document.addEventListener("scroll", () => {
-  scrollToTopButton.style.opacity = window.scrollY > 1500 ? 1 : 0;
-});
-
 function handleScroll() {
   var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
-  if (rootElement.scrollTop / scrollTotal > 0.2) {
+  if (rootElement.scrollTop / scrollTotal > 0.35) {
     scrollToTopButton.style.opacity = 1;
   } else {
     scrollToTopButton.style.opacity = 0;
